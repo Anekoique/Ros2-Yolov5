53b3d3a343866934b0a64056fe8bdabb03ed8001
stoair <2296663099@qq.com>
2024-07-28 22:29:14 +0800

    trt_getEngine

---
+++ getEngine/build/CMakeFiles/get_engine.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/get_engine.dir/src/main.cpp.o"
+  "CMakeFiles/get_engine.dir/src/main.cpp.o.d"
+  "CMakeFiles/get_engine.dir/src/model.cpp.o"
+  "CMakeFiles/get_engine.dir/src/model.cpp.o.d"
+  "get_engine"
+  "get_engine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/get_engine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
